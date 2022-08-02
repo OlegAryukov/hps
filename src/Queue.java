3e551f4b5d63b7a50dc52f5bb8b34fac0e19bbb0
oaryukov <oaryukov@inno.tech>
2022-08-02 19:10:29 +0300

    Queue task

---
+++ src/Queue.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+public class Queue<T> {
+    public LinkedList<T> source;
+
+    public Queue() {
+        this.source = new LinkedList<>();
+        // инициализация внутреннего хранилища очереди
+    }
+
+    public void enqueue(T item) {
+        source.addLast(item);
+        // вставка в хвост
+    }
+
+    public T dequeue() {
+
+        // выдача из головы
+        if (source.size() == 0)
+            return null; // null если очередь пустая
+        return source.getFirst();
+    }
+
+    public int size() {
+        return source.size(); // размер очереди
+    }
+
+}
