97527b44e24ab111ddbf04afd8699f762654546c
oaryukov <oaryukov@inno.tech>
2022-08-15 09:40:34 +0300

    HashTable task

---
+++ src/HashTable.java
@@ -0,0 +1,74 @@
+public class HashTable {
+    public int size;
+    public int step;
+    public String[] slots;
+
+    public HashTable(int sz, int stp) {
+        size = sz;
+        step = stp;
+        slots = new String[size];
+        for (int i = 0; i < size; i++) slots[i] = null;
+    }
+
+    public int hashFun(String value) {
+        return value.hashCode() & (size - 1);
+        // всегда возвращает корректный индекс слота
+//        return 0;
+    }
+
+    public int seekSlot(String value) {
+        int code = hashFun(value);
+        int firstStep = code + step;
+        if (slots[code] == null)
+            return code;
+        if (firstStep < slots.length && slots[firstStep] == null)
+            return firstStep;
+        while (firstStep != code) {
+            if (firstStep + step < slots.length) {
+                firstStep += step;
+            } else {
+                firstStep = firstStep - slots.length + step;
+            }
+            if(slots[firstStep]==null)
+                return firstStep;
+        }
+        // находит индекс пустого слота для значения, или -1
+        return -1;
+    }
+
+    public int put(String value) {
+        int slot = seekSlot(value);
+        if(slot!=-1) {
+            slots[slot] = value;
+            return slot;
+        }
+
+        // записываем значение по хэш-функции
+
+        // возвращается индекс слота или -1
+        // если из-за коллизий элемент не удаётся разместить
+        return -1;
+    }
+
+    public int find(String value) {
+        int slot = hashFun(value);
+        if(slots[slot].equalsIgnoreCase(value))
+            return slot;
+        int firstCode = slot;
+        slot+=step;
+        if (slot < slots.length && slots[slot].equalsIgnoreCase(value))
+            return slot;
+        while (slot != firstCode) {
+            if (slot + step < slots.length) {
+                slot += step;
+            } else {
+                slot = slot - slots.length + step;
+            }
+            if(slots[slot].equalsIgnoreCase(value))
+                return slot;
+        }
+        // находит индекс слота со значением, или -1
+        return -1;
+    }
+}
+
---
+++ test_src/HashTableTest.java
@@ -0,0 +1,45 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+public class HashTableTest {
+
+    @Test
+    public void putTest(){
+        HashTable testHash = new HashTable(17,3);
+        int slot = testHash.seekSlot("test");
+        assertEquals(slot, testHash.put("test"));
+        assertEquals(slot, testHash.find("test"));
+    }
+
+    @Test
+    public void putTestTwice(){
+        HashTable testHash = new HashTable(17,3);
+        int[] slot = new int[6];
+        slot[5] = testHash.put("test");
+        for (int i = 0; i < 5; i++) {
+            slot[i] = testHash.put("test" + i);
+        }
+        for (int i = 0; i < 5; i++) {
+            assertTrue(slot[i] < testHash.size);
+        }
+        assertEquals(slot[5], testHash.find("test"));
+        assertEquals(slot[0], testHash.find("test0"));
+    }
+
+    @Test
+    public void putTestFullMap(){
+        HashTable testHash = new HashTable(17,3);
+        int[] slot = new int[18];
+        for (int i = 0; i < 18; i++) {
+            slot[i] = testHash.put("test" + i);
+        }
+        for (int i = 0; i < 18; i++) {
+            assertTrue(slot[i] < testHash.size);
+        }
+        assertEquals(slot[0], testHash.find("test0"));
+        assertEquals(slot[17], -1);
+    }
+
+}
