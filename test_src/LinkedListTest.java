56f209df2f69a272cd82a93d3a5bfc9c3e0b3e1f
oaryukov <oaryukov@inno.tech>
2022-08-24 15:32:52 +0300

    BloomFilter task

---
+++ test_src/LinkedListTest.java
@@ -0,0 +1,67 @@
+import java.util.ArrayList;
+
+public class LinkedListTest {
+
+//    public static void main(String[] args) {
+//        ArrayList<Integer> al = new ArrayList<>();
+//        al.add(3, 5);
+//        System.out.println(al.get(0));
+//        System.out.println(al.get(1));
+//        System.out.println(al.get(2));
+//        System.out.println(al.get(3));
+//
+//        Node1 one = new Node1(1);
+//        Node1 two = new Node1(2);
+//        Node1 three = new Node1(3);
+//        Node1 four = new Node1(4);
+//        Node1 five = new Node1(5);
+//        Node1 six = new Node1(6);
+//        Node1 seven = new Node1(7);
+//        Node1 eight = new Node1(8);
+//        Node1 nine = new Node1(9);
+//        Node1 secondTwo = new Node1(2);
+//        Node1 thirdTwo = new Node1(2);
+//        Node1 forthTwo = new Node1(2);
+//
+//        LinkedList l2 = new LinkedList();
+//        l2.addInTail(new Node1(2));
+//        l2.addInTail(new Node1(2));
+//        l2.addInTail(new Node1(1));
+//        l2.addInTail(new Node1(3));
+//        l2.addInTail(new Node1(6));
+//
+//        l2.removeAll(2);
+//        System.out.println(l2.remove(6));
+//        System.out.println(l2.remove(3));
+//        l2.removeAll(1);
+//        l2.removeAll(1);
+//        System.out.println(l2.count());
+//        System.out.println(l2.remove(6));
+//        l2.insertAfter(null, new Node1(8));
+//        l2.insertAfter(null, new Node1(9));
+//        System.out.println(l2.count());
+//
+//        LinkedList ll = new LinkedList();
+//
+//        ll.addInTail(one);
+//        ll.addInTail(two);
+//        ll.addInTail(secondTwo);
+//        ll.addInTail(four);
+//
+//        System.out.println(ll.count());
+//
+//        ll.findAll(2).forEach(n -> System.out.print(n.value));
+//        ll.insertAfter(two, three);
+//        System.out.println("\n"+ll.find(3).value);
+//        ll.remove(2);
+//        ll.findAll(2).forEach(n -> System.out.print("\n"+n.value));
+//        ll.insertAfter(one, two);
+//        ll.findAll(2).forEach(n -> System.out.print(n.value));
+//        ll.removeAll(2);
+//        System.out.println(ll.find(2));
+//        ll.insertAfter(one,two);
+//        ll.insertAfter(three,secondTwo);
+//
+//
+//    }
+}
