afd9c881689c4a53d45131dda00ca3037b359838
olegaryukov@gmail.com <Gfhjkm23ft6fk!>
2022-10-25 18:00:36 +0300

    fix style

--- src/recursion/FindSecondMaxNumber.java
+++ src/recursion/FindSecondMaxNumber.java
@@ -9,7 +9,7 @@ public class FindSecondMaxNumber {
             return null;
         if (numbers.size() == 1)
             return numbers.get(0);
-        if (numbers.size()==2)
+        if (numbers.size() == 2)
             return Math.min(numbers.get(0), numbers.get(1));
         int firstMax = Math.max(numbers.get(0), numbers.get(1));
         int secondMax = Math.min(numbers.get(0), numbers.get(1));
@@ -17,12 +17,13 @@ public class FindSecondMaxNumber {
     }
 
     private Integer recurFindSecondNum(List<Integer> numbers, int firstMax, int secondMax, int i) {
+        boolean endRecursionFlag = i == numbers.size() - 1;
         if (numbers.get(i) == secondMax || numbers.get(i) == firstMax)
-            return (i == numbers.size() - 1) ? secondMax : recurFindSecondNum(numbers, firstMax, secondMax, i+1) ;
+            return endRecursionFlag ? secondMax : recurFindSecondNum(numbers, firstMax, secondMax, i + 1);
         if (numbers.get(i) > secondMax && numbers.get(i) < firstMax)
-            return (i == numbers.size() - 1) ? numbers.get(i) : recurFindSecondNum(numbers, firstMax, numbers.get(i), i+1);
-        if(numbers.get(i) > firstMax)
-            return  (i == numbers.size() - 1) ? firstMax : recurFindSecondNum(numbers, numbers.get(i), firstMax, i+1);
-        return (i == numbers.size() - 1) ? secondMax : recurFindSecondNum(numbers, firstMax, secondMax, i + 1);
+            return endRecursionFlag ? numbers.get(i) : recurFindSecondNum(numbers, firstMax, numbers.get(i), i + 1);
+        if (numbers.get(i) > firstMax)
+            return endRecursionFlag ? firstMax : recurFindSecondNum(numbers, numbers.get(i), firstMax, i + 1);
+        return endRecursionFlag ? secondMax : recurFindSecondNum(numbers, firstMax, secondMax, i + 1);
     }
 }
