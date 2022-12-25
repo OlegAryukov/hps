73a6a4f2a08c840efad334e3155dab2c8574384f
olegaryukov@gmail.com <Gfhjkm23ft6fk!>
2022-12-25 15:32:42 +0300

    graph dfs test

--- test_src/secondpart/graph/GraphTest.java
+++ test_src/secondpart/graph/GraphTest.java
@@ -1,6 +1,6 @@
 package secondpart.graph;
 
-import algosecon.graph.SimpleGraph;
+import algosecon.push.SimpleGraph;
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -30,5 +30,47 @@ public class GraphTest {
         Assert.assertFalse(graph.IsEdge(0,2));
         Assert.assertFalse(graph.IsEdge(3,0));
 
+    }
+
+    @Test
+    public void findPathTest() {
+        SimpleGraph graph = new SimpleGraph(6);
+        graph.AddVertex(1);
+        graph.AddVertex(2);
+        graph.AddVertex(3);
+        graph.AddVertex(4);
+        graph.AddVertex(5);
+        graph.AddVertex(6);
+        graph.AddEdge(0, 2);
+        graph.AddEdge(0, 3);
+        graph.AddEdge(2, 1);
+        graph.AddEdge(3,4);
+        graph.AddEdge(1,5);
+
+        Assert.assertEquals(3, graph.DepthFirstSearch(0, 4).size());
+        Assert.assertEquals(4, graph.DepthFirstSearch(0, 5).size());
+        Assert.assertEquals(5, graph.DepthFirstSearch(1, 4).size());
+
+        graph.RemoveEdge(0,3);
+        Assert.assertEquals(0, graph.DepthFirstSearch(0, 4).size());
+
+    }
+
+    @Test
+    public void getEmptyPathTest() {
+        SimpleGraph graph = new SimpleGraph(6);
+        graph.AddVertex(1);
+        graph.AddVertex(2);
+        graph.AddVertex(3);
+        graph.AddVertex(4);
+        graph.AddVertex(5);
+        graph.AddVertex(6);
+
+        graph.AddEdge(0, 2);
+        graph.AddEdge(2, 1);
+        graph.AddEdge(3,4);
+        graph.AddEdge(1,5);
+
+        Assert.assertEquals(0, graph.DepthFirstSearch(0, 4).size());
     }
 }
